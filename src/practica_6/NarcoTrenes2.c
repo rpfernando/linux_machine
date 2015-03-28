68945345be238cdad00bebf73fe9ba661a69f804
Fernando Padilla <rpfernando@ooyala.com>
2015-03-27 20:35:07 -0600

    Add semaphores

--- src/practica_6/NarcoTrenes2.c
+++ src/practica_6/NarcoTrenes2.c
@@ -1,14 +1,14 @@
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include "semaphore.h"
 
 #define CICLOS 10
 
 char *pais[3] = { "Peru", "Bolivia", "Colombia" };
-struct SEMAPHORE *sem;
+sem_t *sem;
 
 void proceso(int i)
 {
@@ -17,14 +17,14 @@ void proceso(int i)
     for (k = 0; k < CICLOS; k++)
     {
         // Llamada waitsem implementada en la parte 3
-        waitsem(sem);
+        semop(sem);
         printf("Entra %s ", pais[i]);
         fflush(stdout);
         sleep(rand() % 3);
         printf("- %s Sale\n", pais[i]);
 
         // Llamada waitsignal implementada en la parte 3
-        signalsem(sem);
+        semctl(sem);
 
         // Espera aleatoria fuera de la sección crítica
         sleep(rand() % 3);
@@ -60,7 +60,7 @@ int main()
     // Incializar el contador del semáforo en 1 una vez que esté
     // en memoria compartida, de manera que solo a un proceso se le
     // permitirá entrar a la sección crítica
-    initsem(sem, 1);
+    semget(sem, 1, 1);
 
     srand(getpid());
 
